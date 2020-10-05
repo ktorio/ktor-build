16da635a5df10b671c39b30ec9474317464947a8
Hadi Hariri <mail@hadihariri.com>
2020-10-05 10:41:01 +0200

    Rename Docs Project to include API docs also

--- .teamcity/subprojects/build/docsamples/ProjectDocSamples.kt
+++ .teamcity/subprojects/build/docsamples/ProjectDocSamples.kt
@@ -4,7 +4,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 
 
 object ProjectDocSamples : Project({
-  id("ProjectKtorDocSamples")
-  name = "Doc Code"
-  description = "Code samples included in Docs"
+  id("ProjectKtorDocs")
+  name = "Docs"
+  description = "Code samples included in Docs and API Docs"
 })
