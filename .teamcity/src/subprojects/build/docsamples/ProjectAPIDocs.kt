1d6c3aac85cebdd213b5235367513b61e0b330a4
Aleksei.Tirman <aleksei.tirman@jetbrains.com>
2020-10-14 17:20:15 +0300

    KTOR-1137 Initial configurations for API docs deployment

---
+++ .teamcity/src/subprojects/build/docsamples/ProjectAPIDocs.kt
@@ -0,0 +1,69 @@
+package subprojects.build.docsamples
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.AbsoluteId
+import jetbrains.buildServer.configs.kotlin.v2019_2.Project
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import subprojects.VCSAPIDocs
+import subprojects.VCSCore
+
+object ProjectAPIDocs : Project({
+    id("ProjectKtorAPIDocs")
+    name = "API Docs"
+
+    vcsRoot(VCSCore)
+    vcsRoot(VCSAPIDocs)
+
+    buildType {
+        id("KtorAPIDocs_Dokka")
+        name = "Build Dokka artifacts"
+
+        vcs {
+            root(VCSCore)
+        }
+        steps {
+            gradle {
+                name = "Run Dokka"
+                tasks = "dokkaWebsite"
+            }
+            script {
+                name = "Get version number"
+                scriptContent = "./gradlew properties --no-daemon --console=plain -q | sed -n 's/^version: //p' > ktor_version.txt"
+            }
+        }
+    }
+
+    buildType {
+        id("KtorAPIDocs_Deploy")
+        name = "Deploy website"
+
+        vcs {
+            root(VCSAPIDocs)
+        }
+
+        dependencies {
+            dependency(AbsoluteId("KtorAPIDocs_Dokka")) {
+                snapshot {
+                }
+
+                artifacts {
+                    artifactRules = """
+                        apiDoc/**=>apiDoc
+                        ktor_version.txt=>ktor_version.txt
+                    """.trimIndent()
+                }
+            }
+        }
+
+        steps {
+            script {
+                name = "Create pull request on Github"
+                scriptContent = """
+                    ls -la
+                    ls -la apiDoc
+                    cat ktor_version.txt
+                """.trimIndent()
+            }
+        }
+    }
+})
