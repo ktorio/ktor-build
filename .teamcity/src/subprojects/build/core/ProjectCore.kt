f9527baa025f8f0187e02e63485a8dafce92d365
Hadi Hariri <mail@hadihariri.com>
2020-10-15 11:08:30 +0200

    KTOR-1114 Add support for stress tests

--- .teamcity/src/subprojects/build/core/ProjectCore.kt
+++ .teamcity/src/subprojects/build/core/ProjectCore.kt
@@ -6,11 +6,13 @@ import subprojects.*
 data class JDKEntry(val name: String, val env: String)
 data class OSEntry(val name: String, val agentString: String, val taskName: String)
 data class JavaScriptEngine(val name: String, val dockerContainer: String)
-data class CoreEntry(val osEntry: OSEntry, val jdkEntry: JDKEntry)
+data class OSJVMComboEntry(val osEntry: OSEntry, val jdkEntry: JDKEntry)
 
 val operatingSystems = listOf(OSEntry("macOS", "Mac OS X", "linkDebugTestMacosX64"), OSEntry("Linux", "Linux", "linkDebugTestLinuxX64"), OSEntry("Windows", "Windows", "linkDebugTestMingwX64"))
 val jdkVersions = listOf(JDKEntry("Java 8", "JDK_18"), JDKEntry("Java 11", "JDK_11"))
 val javaScriptEngines = listOf(JavaScriptEngine("Chrome/Node.js", "stl5/ktor-test-image:latest"))
+val stressTests = listOf(OSJVMComboEntry(OSEntry("Linux", "Linux", "linkDebugTestLinuxX64"), JDKEntry("Java 8", "JDK_18")),
+    OSJVMComboEntry(OSEntry("Windows", "Windows", "linkDebugTestMingwX64"), JDKEntry("Java 8", "JDK_18")))
 
 
 object ProjectCore : Project({
@@ -24,14 +26,15 @@ object ProjectCore : Project({
     }
 
     val osJVMCombos = operatingSystems.flatMap { os ->
-        jdkVersions.map { jdk -> CoreEntry(os, jdk) }
+        jdkVersions.map { jdk -> OSJVMComboEntry(os, jdk) }
     }
 
     val osJVMBuilds = osJVMCombos.map(::CoreBuild)
     val nativeBuilds = operatingSystems.map(::NativeBuild)
     val javaScriptBuilds = javaScriptEngines.map(::JavaScriptBuild)
+    val stressTestBuilds = stressTests.map(::StressTestBuild)
 
-    val allBuilds = osJVMBuilds.plus(nativeBuilds).plus(javaScriptBuilds)
+    val allBuilds = osJVMBuilds.plus(nativeBuilds).plus(javaScriptBuilds).plus(stressTestBuilds)
 
     allBuilds.forEach(::buildType)
 
---
+++ .teamcity/src/subprojects/build/core/StressTestBuild.kt
@@ -0,0 +1,42 @@
+package subprojects.build.core
+
+import jetbrains.buildServer.configs.kotlin.v10.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.*
+import subprojects.*
+
+class StressTestBuild(private val osJVMComboEntry: OSJVMComboEntry) : BuildType({
+    id("KtorMatrixStressTest_${osJVMComboEntry.osEntry.name}${osJVMComboEntry.jdkEntry.name}".toExtId())
+    name = "Stress Test on ${osJVMComboEntry.osEntry.name} and ${osJVMComboEntry.jdkEntry.name}"
+    vcs {
+        root(VCSCore)
+    }
+    triggers {
+        schedule {
+            schedulingPolicy = daily {
+                hour = 8
+                timezone = "Europe/Moscow"
+            }
+            branchFilter = """
+                    +:master
+                    """.trimIndent()
+            triggerBuild = always()
+            param("revisionRuleBuildBranch", "<default>")
+        }
+    }
+    steps {
+        gradle {
+            name = "Run stress tests"
+            tasks = "stressTest --info"
+            jdkHome = "%${osJVMComboEntry.jdkEntry.env}%"
+        }
+    }
+    features {
+        setupPerformanceMonitoring()
+    }
+    requirements {
+        noLessThan("teamcity.agent.hardware.memorySizeMb", "7000")
+        contains("teamcity.agent.jvm.os.name", osJVMComboEntry.osEntry.agentString)
+    }
+})
