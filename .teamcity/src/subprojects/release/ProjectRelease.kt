3971352f11139d3aa38fdb8cad420078a323a885
Hadi Hariri <mail@hadihariri.com>
2020-10-26 14:34:56 +0100

    KTOR-1170 Private Keys

--- .teamcity/src/subprojects/release/ProjectRelease.kt
+++ .teamcity/src/subprojects/release/ProjectRelease.kt
@@ -8,5 +8,11 @@ object ProjectRelease : Project({
     name = "Release"
     description = "Build configuration that release Ktor"
 
+    params {
+        param("sign.key.id", value = "7C30F7B1329DBA87")
+        password("sign.key.passphrase", value = "credentialsJSON:59f4247e-21d9-4354-a0f0-3051fd16ef5d")
+        password("sign.key.private", value = "credentialsJSON:1196162d-f166-4302-b179-6e463bc5c327")
+    }
+
     subProject(ProjectReleaseAPIDocs)
 })
