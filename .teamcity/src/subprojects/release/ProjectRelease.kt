0dc89de392ccd46007fa040ebae2f50afea1cf03
Hadi Hariri <mail@hadihariri.com>
2020-10-27 08:09:38 +0100

    KTOR-1170 Convert to env variables.

--- .teamcity/src/subprojects/release/ProjectRelease.kt
+++ .teamcity/src/subprojects/release/ProjectRelease.kt
@@ -9,11 +9,11 @@ object ProjectRelease : Project({
     description = "Build configuration that release Ktor"
 
     params {
-        param("sign.key.id", value = "7C30F7B1329DBA87")
-        password("sign.key.passphrase", value = "credentialsJSON:59f4247e-21d9-4354-a0f0-3051fd16ef5d")
-        password("sign.key.private", value = "credentialsJSON:1196162d-f166-4302-b179-6e463bc5c327")
-        password("sonatypeUser", value = "credentialsJSON:1809dc95-c346-410a-931b-3e1c6cea58cc")
-        password("sonatypePassword", value = "credentialsJSON:c8be43cb-031a-4679-858e-305e47b3368a")
+        param("env.SIGN_KEY_ID", value = "7C30F7B1329DBA87")
+        password("env.SIGN_KEY_PASSPHRASE", value = "credentialsJSON:59f4247e-21d9-4354-a0f0-3051fd16ef5d")
+        password("env.SIGN_KEY_PRIVATE", value = "credentialsJSON:1196162d-f166-4302-b179-6e463bc5c327")
+        password("env.SONATYPE_USER", value = "credentialsJSON:1809dc95-c346-410a-931b-3e1c6cea58cc")
+        password("env.SONATYPE_PASSWORD", value = "credentialsJSON:c8be43cb-031a-4679-858e-305e47b3368a")
     }
 
     subProject(ProjectReleaseAPIDocs)
