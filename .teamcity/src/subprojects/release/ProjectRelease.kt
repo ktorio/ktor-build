b0cd372aaa05754d0229fc5efb90c277fea77cf7
Hadi Hariri <mail@hadihariri.com>
2020-10-31 11:35:12 +0100

    Update key id

--- .teamcity/src/subprojects/release/ProjectRelease.kt
+++ .teamcity/src/subprojects/release/ProjectRelease.kt
@@ -20,7 +20,7 @@ object ProjectRelease : Project({
     params {
         defaultTimeouts()
         text("release.version", "", display = ParameterDisplay.PROMPT, allowEmpty = false)
-        param("env.SIGN_KEY_ID", value = "7C30F7B1329DBA87")
+        param("env.SIGN_KEY_ID", value = "0x7c30f7b1329dba87")
         password("env.SIGN_KEY_PASSPHRASE", value = "credentialsJSON:59f4247e-21d9-4354-a0f0-3051fd16ef5d")
         password("env.SIGN_KEY_PRIVATE", value = "credentialsJSON:1196162d-f166-4302-b179-6e463bc5c327")
         password("env.SONATYPE_USER", value = "credentialsJSON:1809dc95-c346-410a-931b-3e1c6cea58cc")
