faebc2c96e755dcc690a99fb5d020541e5b33fa8
Hadi Hariri <mail@hadihariri.com>
2020-11-03 20:13:59 +0100

    More tests

--- .teamcity/src/subprojects/release/publishing/PublishMavenBuilds.kt
+++ .teamcity/src/subprojects/release/publishing/PublishMavenBuilds.kt
@@ -64,7 +64,6 @@ object PublishWindowsNativeToMaven : BuildType({
             scriptContent = """    
 choco uninstall -y gpg4win
 choco install -y gnupg
-GNUPGHOME=C:\Users\builduser\AppData\Roaming\gnupg
             """.trimIndent()
         }
         publishToMaven(
