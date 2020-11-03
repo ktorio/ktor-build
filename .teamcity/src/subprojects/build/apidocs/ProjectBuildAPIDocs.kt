7ac37417ad8a6e6b6c8311cde459916801c3646d
Hadi Hariri <mail@hadihariri.com>
2020-11-03 08:59:13 +0100

    Revert "Extract build to own file"
    This reverts commit b2d107f3

--- .teamcity/src/subprojects/build/apidocs/ProjectBuildAPIDocs.kt
+++ .teamcity/src/subprojects/build/apidocs/ProjectBuildAPIDocs.kt
@@ -1,7 +1,10 @@
 package subprojects.build.apidocs
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.*
+import subprojects.*
 import subprojects.build.*
+import subprojects.build.core.*
 import subprojects.release.*
 
 object ProjectBuildAPIDocs : Project({
@@ -15,3 +18,24 @@ object ProjectBuildAPIDocs : Project({
     buildType(apiBuild ?: throw RuntimeException("ProjectBuildApiDocs is null"))
 })
 
+object BuildDokka: BuildType({
+    id("KtorAPIDocs_Dokka")
+    name = "Build Dokka artifacts"
+
+    vcs {
+        root(VCSCore)
+    }
+
+    requirements {
+        require(os = "Linux", minMemoryMB = 7000)
+    }
+
+    artifactRules = formatArtifacts("+:apidoc => apidoc.zip")
+
+    steps {
+        gradle {
+            name = "Run Dokka"
+            tasks = "dokkaWebsite"
+        }
+    }
+})
