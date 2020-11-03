b2d107f376f46b2d4600ad11f08c9146eec7c8b5
Hadi Hariri <mail@hadihariri.com>
2020-11-03 08:29:00 +0100

    Extract build to own file

--- .teamcity/src/subprojects/build/apidocs/ProjectBuildAPIDocs.kt
+++ .teamcity/src/subprojects/build/apidocs/ProjectBuildAPIDocs.kt
@@ -1,10 +1,7 @@
 package subprojects.build.apidocs
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.*
-import subprojects.*
 import subprojects.build.*
-import subprojects.build.core.*
 import subprojects.release.*
 
 object ProjectBuildAPIDocs : Project({
@@ -18,24 +15,3 @@ object ProjectBuildAPIDocs : Project({
     buildType(apiBuild ?: throw RuntimeException("ProjectBuildApiDocs is null"))
 })
 
-object BuildDokka: BuildType({
-    id("KtorAPIDocs_Dokka")
-    name = "Build Dokka artifacts"
-
-    vcs {
-        root(VCSCore)
-    }
-
-    requirements {
-        require(os = "Linux", minMemoryMB = 7000)
-    }
-
-    artifactRules = formatArtifacts("+:apidoc => apidoc.zip")
-
-    steps {
-        gradle {
-            name = "Run Dokka"
-            tasks = "dokkaWebsite"
-        }
-    }
-})
