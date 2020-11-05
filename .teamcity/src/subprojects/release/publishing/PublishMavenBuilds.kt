9a5c37acfbb1424ffbdd0a5f60e059acad178a9f
Hadi Hariri <mail@hadihariri.com>
2020-11-05 10:20:01 +0100

    Add message to removing folder

--- .teamcity/src/subprojects/release/publishing/PublishMavenBuilds.kt
+++ .teamcity/src/subprojects/release/publishing/PublishMavenBuilds.kt
@@ -148,6 +148,7 @@ fun BuildSteps.prepareKeyFile(os: String = "") {
                     content = """
 md ${'$'}Env:SIGN_KEY_LOCATION -force
 cd ${'$'}Env:SIGN_KEY_LOCATION
+echo "Removing GNUPG folder"
 rm -r -fo C:\Users\builduser\AppData\Roaming\gnupg\
 
 # Hard-coding path for GPG since this fails on TeamCity
