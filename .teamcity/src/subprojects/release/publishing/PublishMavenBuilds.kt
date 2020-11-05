90dcb84defb121bfce5b929a86700a0e0e7f310f
nkoshcheev <29865719+nkoshcheev@users.noreply.github.com>
2020-11-05 22:32:37 +0300

    correct gpg-connect-agent invocation

--- .teamcity/src/subprojects/release/publishing/PublishMavenBuilds.kt
+++ .teamcity/src/subprojects/release/publishing/PublishMavenBuilds.kt
@@ -168,7 +168,7 @@ echo "Exporting private key"
 & ${'$'}gpg --allow-secret-key-import --batch --import keyfile
 rm keyfile
 & ${'$'}gpg --list-keys
-& ${'$'}gpg-connect-agent /bye
+& "gpg-connect-agent" /bye
 
             """.trimIndent()
                 }
