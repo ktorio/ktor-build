bf99a8d71e2b72cbc3daa7824d4fad4f19a18ac0
Hadi Hariri <mail@hadihariri.com>
2021-02-05 11:27:32 +0100

    Add agent requirements

--- .teamcity/src/subprojects/eap/SetBuildNumber.kt
+++ .teamcity/src/subprojects/eap/SetBuildNumber.kt
@@ -1,10 +1,15 @@
 package subprojects.eap
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import subprojects.build.*
+import subprojects.build.core.*
 
 
 object SetBuildNumber: BuildType( {
     id("SetBuildNumberBuild")
     name = "Set EAP Build Number"
     buildNumberPattern = eapVersion
+    requirements {
+        require(linux.agentString)
+    }
 })
