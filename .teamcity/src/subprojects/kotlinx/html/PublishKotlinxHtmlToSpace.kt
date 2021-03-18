b370d6c8c2f4069c1d32a4499da6a36d4b7c1878
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2021-03-18 12:59:36 +0300

    KTOR-2206 Add signing keys configuration

--- .teamcity/src/subprojects/kotlinx/html/PublishKotlinxHtmlToSpace.kt
+++ .teamcity/src/subprojects/kotlinx/html/PublishKotlinxHtmlToSpace.kt
@@ -3,20 +3,18 @@ package subprojects.kotlinx.html
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.vcsLabeling
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
-import subprojects.VCSCore
-import subprojects.VCSCoreEAP
-import subprojects.VCSKotlinxHtml
+import subprojects.*
 import subprojects.build.core.require
 import subprojects.build.defaultTimeouts
 import subprojects.build.java11
 import subprojects.build.linux
-import subprojects.defaultBranch
 import subprojects.eap.SetBuildNumber
 import subprojects.release.configureReleaseVersion
 import subprojects.release.createDeploymentBuild
 import subprojects.release.publishing.cleanupKeyFile
 import subprojects.release.publishing.prepareKeyFile
 import subprojects.release.publishing.releaseVersion
+import java.io.File
 
 object PublishKotlinxHtmlToSpace : Project({
     id("ProjectKotlinxHtmlToSpace")
@@ -28,6 +26,11 @@ object PublishKotlinxHtmlToSpace : Project({
         password("env.PUBLISHING_USER", value = "%space.packages.kotlinx.html.user%")
         password("env.PUBLISHING_PASSWORD", value = "%space.packages.kotlinx.html.secret%")
         param("env.PUBLISHING_URL", value = "%space.packages.kotlinx.html.url%")
+
+        password("env.SIGN_KEY_PASSPHRASE", value = "%sign.key.passphrase%")
+        password("env.SIGN_KEY_PRIVATE", value = "%sign.key.private%")
+        param("env.SIGN_KEY_LOCATION", value = File("%teamcity.build.checkoutDir%").invariantSeparatorsPath)
+        param("env.SIGN_KEY_PUBLIC", value = SIGN_KEY_PUBLIC)
     }
 
     buildType {
