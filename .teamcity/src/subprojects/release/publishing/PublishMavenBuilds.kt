f9bfe878b4df8ecf1f1ca7bb3f6c99cccc0c0439
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-10-26 16:12:13 +0200

    KTOR-3144 Add publishing for Apple Silicon targets (#68)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- .teamcity/src/subprojects/release/publishing/PublishMavenBuilds.kt
+++ .teamcity/src/subprojects/release/publishing/PublishMavenBuilds.kt
@@ -138,13 +138,20 @@ object PublishMacOSNativeToMaven : BuildType({
                 "publishIosArm32PublicationToMavenRepository",
                 "publishIosArm64PublicationToMavenRepository",
                 "publishIosX64PublicationToMavenRepository",
+                "publishIosSimulatorArm64PublicationToMavenRepository",
+
                 "publishMacosX64PublicationToMavenRepository",
+                "publishMacosArm64PublicationToMavenRepository",
+
                 "publishTvosArm64PublicationToMavenRepository",
                 "publishTvosX64PublicationToMavenRepository",
+                "publishTvosSimulatorArm64PublicationToMavenRepository",
+
                 "publishWatchosArm32PublicationToMavenRepository",
                 "publishWatchosArm64PublicationToMavenRepository",
                 "publishWatchosX86PublicationToMavenRepository",
-                "publishWatchosX64PublicationToMavenRepository"
+                "publishWatchosX64PublicationToMavenRepository",
+                "publishWatchosSimulatorArm64PublicationToMavenRepository"
             ),
             gradleParams = "-Psigning.gnupg.executable=gpg -Psigning.gnupg.homeDir=%env.SIGN_KEY_LOCATION%/.gnupg"
         )
