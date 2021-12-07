2353561235738fd2f457cde87a7d8dc460f8e88c
leonid.stashevsky <leonid.stashevsky@gmail.com>
2021-12-07 12:58:37 +0100

    Drop Jdk7 installation

--- .teamcity/src/subprojects/build/core/CoreBuild.kt
+++ .teamcity/src/subprojects/build/core/CoreBuild.kt
@@ -24,10 +24,8 @@ class CoreBuild(private val osJdkEntry: OSJDKEntry) : BuildType({
         when (osJdkEntry.osEntry) {
             windows -> {
                 defineTCPPortRange()
-                installJdk7OnWindows()
             }
             linux -> {
-                installJdk7OnLinux()
                 script {
                     name = "Obtain Library Dependencies"
                     scriptContent = libSoftware
@@ -61,76 +59,4 @@ fun BuildSteps.defineTCPPortRange() {
 
 fun formatArtifacts(vararg artifacts: String): String {
     return artifacts.joinToString("\n")
-}
-
-fun BuildSteps.installJdk7OnLinux() {
-    val VERSION = "\$VERSION"
-    val ARCH = "\$ARCH"
-    val DIR = "\$DIR"
-    val FILE = "\$FILE"
-
-    val script = """
-        #!/bin/bash
-
-        export AWS_DEFAULT_REGION=eu-west-1
-        aws s3 cp s3://common-teamcityhostedprod/plugins/compassionate-ptolemy-6wkpld/jdk-7.79-linux_x64.tar.gz ./ --only-show-errors
-
-        VERSION=7.79
-        ARCH=x64
-        FILE=jdk-$VERSION-linux_$ARCH.tar.gz
-        DIR="%system.agent.home.dir%/jdk-$VERSION-$ARCH"
-        mkdir -p ~/bin
-
-        [[ -d "$DIR" ]] && rm -rf "$DIR"
-        mkdir -p "$DIR"
-        tar -xzf "./$FILE" --strip-components=1 -C "$DIR"
-        rm "./$FILE"
-
-        cd $DIR/bin
-        ./java -version
-
-        echo "##teamcity[setParameter name='env.JDK_1_7' value='${'$'}(pwd)']"
-    """.trimIndent()
-
-    script {
-        name = "Install JDK7 on Linux"
-        scriptContent = script
-    }
-}
-
-fun BuildSteps.installJdk7OnWindows() {
-    val D = "\$"
-    val scriptContent = """
-        
-        ${D}version = "7.79"
-        ${D}arch = "x64"
-        ${D}file = "jdk-${D}version-windows_${D}arch.exe"
-        ${D}url = "s3://common-teamcityhostedprod/plugins/compassionate-ptolemy-6wkpld/${D}file"
-
-        Write-Host "* Downloading JDK from S3 bucket ${D}url"
-        ${D}path = "${D}env:temp\${'$'}file"
-        aws s3 cp ${D}url ${D}path --only-show-errors
-
-        ${D}jdkdir = "C:\tools\jdk${D}version-${D}arch"
-
-        Write-Host "* Installing JDK ${D}version (${D}arch)"
-        ${D}args = "/s ADDLOCAL=ToolsFeature INSTALLDIR=${D}jdkdir"
-        ${D}process = (Start-Process -Wait -PassThru ${D}path -ArgumentList ${D}args)
-        ${D}process.WaitForExit()
-        if (${D}process.ExitCode -ne 0) {
-            throw "Exec: Unable to install jdk: exit code " + ${D}process.ExitCode
-        }
-        echo "##teamcity[setParameter name='env.JDK_1_7' value='${D}jdkdir\bin']"
-    """.trimIndent()
-
-    powerShell {
-        name = "Install JDK7 on Windows"
-        scriptMode = PowerShellStep.ScriptMode.Script().apply {
-            content = scriptContent
-        }
-
-        script {
-            content = scriptContent
-        }
-    }
 }
