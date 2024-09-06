d7c3c62e76a7de78b19f5d64b6a70af132c2b992
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2024-09-06 15:37:12 +0300

    [Ktor CLI] Fix extensions for windows

--- .teamcity/src/subprojects/cli/ProjectKtorCLI.kt
+++ .teamcity/src/subprojects/cli/ProjectKtorCLI.kt
@@ -91,8 +91,8 @@ assets = {
     f"ktor_linux_arm64": 'build/linux/arm64/ktor',
     f"ktor_macos_amd64": 'build/darwin/amd64/ktor',
     f"ktor_macos_arm64": 'build/darwin/arm64/ktor',
-    f"ktor_windows_amd64": 'build/windows/amd64/ktor.exe',
-    f"ktor_windows_installer_amd64": 'build/windows/amd64/ktor-installer.msi',
+    f"ktor.exe": 'build/windows/amd64/ktor.exe',
+    f"ktor_installer.msi": 'build/windows/amd64/ktor-installer.msi',
 }
 
 for name, filepath in assets.items():
