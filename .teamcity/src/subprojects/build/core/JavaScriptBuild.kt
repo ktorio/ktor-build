47ecac5123a6abf15ef8bc7c05375daafad04a90
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2024-11-22 16:44:39 +0100

    Specify jdkHome for builds where it was missing

--- .teamcity/src/subprojects/build/core/JavaScriptBuild.kt
+++ .teamcity/src/subprojects/build/core/JavaScriptBuild.kt
@@ -23,6 +23,7 @@ class JavaScriptBuild(private val jsEntry: JSEntry) : BuildType({
         gradle {
             name = "Build Js"
             tasks = "cleanJsTest jsTest --no-parallel --continue --info -Penable-js-tests"
+            jdkHome = Env.JDK_LTS
             setupDockerForJavaScriptTests(jsEntry)
         }
     }
