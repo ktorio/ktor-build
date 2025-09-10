56cd2aa944e8415c50c93b9e5fac229424ada2f4
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2025-09-10 11:38:48 +0200

    fixup! Add system

---
+++ .teamcity/scripts/check_modified_rust_modules.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+CHANGED_FILES="%system.teamcity.build.changedFiles.file%"
+
+# Check if any files match your path pattern
+if echo "$CHANGED_FILES" | grep -q "\-rs"; then
+    echo "Changes detected for files with '-rs' in their path"
+    export KTOR_RUST_COMPILATION="true"
+    echo "##teamcity[setParameter name='env.KTOR_RUST_COMPILATION' value='${'$'}KTOR_RUST_COMPILATION']"
+fi
+
+# Set platform-specific Konan properties for Linux
+if [ "$OPERATING_SYSTEM" = "Linux" ] && [ "$KTOR_RUST_COMPILATION" = "true" ]; then
+    echo "Setting Linux-specific Konan properties"
+    export KTOR_OVERRIDE_KONAN_PROPERTIES="targetSysRoot.linux_x64=/;libGcc.linux_x64=/usr/lib/gcc/x86_64-linux-gnu/13;linker.linux_x64=/usr/bin/ld.bfd;crtFilesLocation.linux_x64=/usr/lib/x86_64-linux-gnu/"
+    echo "##teamcity[setParameter name='env.KTOR_OVERRIDE_KONAN_PROPERTIES' value='${'$'}KTOR_OVERRIDE_KONAN_PROPERTIES']"
+fi
--- .teamcity/src/subprojects/build/core/RustUtils.kt
+++ .teamcity/src/subprojects/build/core/RustUtils.kt
@@ -20,30 +20,13 @@ fun BuildSteps.installRust(os: OS) {
 }
 
 fun BuildType.enableRustForRelevantChanges(os: OS) {
+    params {
+        param("env.OPERATING_SYSTEM", os.name)
+    }
     steps {
         script {
             name = "Check for Rust module changes"
-            scriptContent = """
-                #!/bin/bash
-                
-                CHANGED_FILES="%system.teamcity.build.changedFiles.file%"
-                OPERATING_SYSTEM="${os.name}"
-               
-                # Check if any files match your path pattern
-                if echo "${'$'}CHANGED_FILES" | grep -q "-rs"; then
-                    echo "Changes detected for files with '-rs' in their path"
-                    export KTOR_RUST_COMPILATION="true"
-                    echo "##teamcity[setParameter name='env.KTOR_RUST_COMPILATION' value='${'$'}KTOR_RUST_COMPILATION']"
-                fi 
-                
-                # Set platform-specific Konan properties for Linux
-                if [ "${'$'}OPERATING_SYSTEM" = "Linux" ] && [ "${'$'}KTOR_RUST_COMPILATION" = "true" ]; then
-                    echo "Setting Linux-specific Konan properties"
-                    export KTOR_OVERRIDE_KONAN_PROPERTIES="targetSysRoot.linux_x64=/;libGcc.linux_x64=/usr/lib/gcc/x86_64-linux-gnu/13;linker.linux_x64=/usr/bin/ld.bfd;crtFilesLocation.linux_x64=/usr/lib/x86_64-linux-gnu/"
-                    echo "##teamcity[setParameter name='env.KTOR_OVERRIDE_KONAN_PROPERTIES' value='${'$'}KTOR_OVERRIDE_KONAN_PROPERTIES']"
-                fi
-            """.trimIndent()
-            executionMode = BuildStep.ExecutionMode.ALWAYS
+            scriptFile("check_modified_rust_modules.sh")
         }
     }
 }
