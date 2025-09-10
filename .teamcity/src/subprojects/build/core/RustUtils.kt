3d673814de37857c9f42841392483cb0adc1d17a
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2025-09-10 10:51:34 +0200

    fixup! Enable rust only when module is changed

--- .teamcity/src/subprojects/build/core/RustUtils.kt
+++ .teamcity/src/subprojects/build/core/RustUtils.kt
@@ -41,8 +41,8 @@ fun BuildSteps.enableRustForRelevantChanges() {
             # Set platform-specific Konan properties for Linux
             if [ "${'$'}OPERATING_SYSTEM" = "Linux" ] && [ "${'$'}KTOR_RUST_COMPILATION" = "true" ]; then
                 echo "Setting Linux-specific Konan properties"
-                export KONAN_PROPERTIES="targetSysRoot.linux_x64=/;libGcc.linux_x64=/usr/lib/gcc/x86_64-linux-gnu/13;linker.linux_x64=/usr/bin/ld.bfd;crtFilesLocation.linux_x64=/usr/lib/x86_64-linux-gnu/"
-                echo "##teamcity[setParameter name='env.KTOR_OVERRIDE_KONAN_PROPERTIES' value='${'$'}KONAN_PROPERTIES']"
+                export KTOR_OVERRIDE_KONAN_PROPERTIES="targetSysRoot.linux_x64=/;libGcc.linux_x64=/usr/lib/gcc/x86_64-linux-gnu/13;linker.linux_x64=/usr/bin/ld.bfd;crtFilesLocation.linux_x64=/usr/lib/x86_64-linux-gnu/"
+                echo "##teamcity[setParameter name='env.KTOR_OVERRIDE_KONAN_PROPERTIES' value='${'$'}KTOR_OVERRIDE_KONAN_PROPERTIES']"
             fi
         """
         executionMode = BuildStep.ExecutionMode.ALWAYS
