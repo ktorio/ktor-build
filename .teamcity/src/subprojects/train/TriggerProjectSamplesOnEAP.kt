5742f1ed6d396968be23a07ee67b4053627f49cf
Violetta Goncharova <40871415+VGoncharova@users.noreply.github.com>
2025-10-30 09:46:03 +0100

    update Ktor EAP trigger settings: adjust Gradle wrapper path for non-standalone sample builds

--- .teamcity/src/subprojects/train/TriggerProjectSamplesOnEAP.kt
+++ .teamcity/src/subprojects/train/TriggerProjectSamplesOnEAP.kt
@@ -227,7 +227,7 @@ fun BuildSteps.buildEAPGradlePluginSample(relativeDir: String, standalone: Boole
         tasks = "build"
         workingDir = if (!standalone) "samples/$relativeDir" else ""
         useGradleWrapper = true
-        gradleWrapperPath = "gradlew"
+        gradleWrapperPath = if (!standalone) "../../gradlew" else "gradlew"
         gradleParams = "--init-script=%system.teamcity.build.tempDir%/ktor-eap.init.gradle.kts"
         jdkHome = Env.JDK_LTS
         executionMode = BuildStep.ExecutionMode.RUN_ON_SUCCESS
