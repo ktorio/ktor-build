9944eb3e184fc3d35df4ea899a6bd74a0b299cca
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2025-11-03 17:15:38 +0100

    Doc samples: Specify agent OS and arch

--- .teamcity/src/subprojects/build/docsamples/ProjectDocSamples.kt
+++ .teamcity/src/subprojects/build/docsamples/ProjectDocSamples.kt
@@ -3,34 +3,41 @@ package subprojects.build.docsamples
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.*
 import subprojects.*
+import subprojects.Agents.ANY
+import subprojects.Agents.Arch
+import subprojects.Agents.OS
 import subprojects.release.*
 
 const val relativeDir = "codeSnippets"
 
 object ProjectDocSamples : Project({
-  id("ProjectKtorDocs")
-  name = "Docs"
-  description = "Code samples included in Docs and API Docs"
+    id("ProjectKtorDocs")
+    name = "Docs"
+    description = "Code samples included in Docs and API Docs"
 
-  samplesBuild = buildType {
-    id("KtorDocs_ValidateSamples")
-    name = "Build and test code samples"
+    samplesBuild = buildType {
+        id("KtorDocs_ValidateSamples")
+        name = "Build and test code samples"
 
-    vcs {
-      root(VCSDocs)
+        vcs {
+            root(VCSDocs)
+        }
+
+        requirements {
+            agent(OS.Linux, Arch.X64, hardwareCapacity = ANY)
+        }
+
+        steps {
+            gradle {
+                name = "Build"
+                tasks = "clean build"
+                workingDir = relativeDir
+            }
+            gradle {
+                name = "Test"
+                tasks = "test"
+                workingDir = relativeDir
+            }
+        }
     }
-
-    steps {
-      gradle {
-        name = "Build"
-        tasks = "clean build"
-        workingDir = relativeDir
-      }
-      gradle {
-        name = "Test"
-        tasks = "test"
-        workingDir = relativeDir
-      }
-    }
-  }
 })
