8ea43b8d6b7cb21dc9707e7a6fbd5efd880c7c54
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2026-01-23 01:30:20 +0100

    Add cache and retries

--- .teamcity/src/subprojects/eap/SyncPublicJetbrainsSpaceToJetbrainsTeam.kt
+++ .teamcity/src/subprojects/eap/SyncPublicJetbrainsSpaceToJetbrainsTeam.kt
@@ -1,49 +1,48 @@
 package subprojects.eap
 
+import dsl.*
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.*
+import jetbrains.buildServer.configs.kotlin.triggers.*
 import subprojects.*
 
 object SyncPublicJetbrainsSpaceToJetbrainsTeam : BuildType({
     name = "Sync repo: public.jetbrains.space -> jetbrains.team"
     description = "Uploads all artifacts from source repository to target"
 
-    artifactRules = "space => space.zip"
-
     params {
         text("space-url", "https://public.jetbrains.space", display = ParameterDisplay.PROMPT, allowEmpty = true)
-        text("space-username", "%space.public.packages.user%", display = ParameterDisplay.PROMPT, allowEmpty = true)
         password("space-password", "%space.public.packages.secret%", display = ParameterDisplay.PROMPT)
         text("from-repo-name", "eap", description = "Name of the source repository that will be used", display = ParameterDisplay.PROMPT, allowEmpty = true)
         text("from-project-key", "ktor", description = "Name of the source project that will be used. Expected https://public.jetbrains.space/p/ktor/packages but you can use other if you have right permissions.", display = ParameterDisplay.PROMPT, allowEmpty = true)
 
         param("to-space-url", "https://jetbrains.team")
-        param("to-space-username", "%space.packages.user%")
         param("to-space-password", "%space.packages.secret%")
         text("to-repo-name", "eap", description = "Name of the target repository that will be used", display = ParameterDisplay.PROMPT, allowEmpty = true)
         text("to-project-key", "ktor", description = "Name of the target project that will be used. Expected https://jetbrains.team/p/ktor/packages, but you can use other if you have right permissions.", display = ParameterDisplay.PROMPT, allowEmpty = true)
+
+        text("versions", "all", display = ParameterDisplay.PROMPT)
     }
 
     steps {
         script {
             name = "Execute"
-            //language=bash
-            scriptContent = """
-                publishing-utils \
-                 space-sync \
+            scriptContent = bashScript("""
+                space-cli \
+                  sync \
                   --space-url=%space-url% \
                   --space-password=%space-password% \
                   --to-space-url=%to-space-url% \
-                  --to-space-username=%to-space-username% \
                   --to-space-password=%to-space-password% \
                   --from-project-key=%from-project-key% \
                   --from-repo-name=%from-repo-name% \
                   --to-project-key=%to-project-key% \
                   --to-repo-name=%to-repo-name% \
-                  --versions=all
-            """.trimIndent()
-            dockerImage = "registry.jetbrains.team/p/kti/containers/publishing-utils:188"
+                  --versions=%versions%
+            """)
+            dockerImage = "registry.jetbrains.team/p/ktor/containers/space-cli:latest"
+            dockerRunParameters = "-v %teamcity.build.checkoutDir%/.space-sync-cache:/app/.space-sync-cache"
         }
         script {
             name = "Print build status"
@@ -62,8 +61,27 @@ object SyncPublicJetbrainsSpaceToJetbrainsTeam : BuildType({
         }
     }
 
+    artifactRules = "+:.space-sync-cache/** => space-sync-cache.zip"
+    publishArtifacts = PublishMode.ALWAYS
+
+    dependencies {
+        artifacts(SyncPublicJetbrainsSpaceToJetbrainsTeam.id!!) {
+            buildRule = lastFinished()
+            artifactRules = "?:space-sync-cache.zip!** => .space-sync-cache"
+        }
+    }
+
+    triggers {
+        retryBuild {
+            moveToTheQueueTop = true
+            retryWithTheSameRevisions = false
+            attempts = Int.MAX_VALUE
+            branchFilter = BranchFilter.AllBranches
+        }
+    }
+
     requirements {
-        agent(Agents.OS.Linux)
+        agent(Agents.OS.Linux, Agents.Arch.Arm64)
     }
 
     cleanup {
