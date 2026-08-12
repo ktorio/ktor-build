d7e3d2083a69c8dd23f5805e823169ef3524c5fc
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2026-08-12 13:28:04 +0200

    Split allocation benchmark build

--- .teamcity/src/subprojects/benchmarks/ProjectBenchmarks.kt
+++ .teamcity/src/subprojects/benchmarks/ProjectBenchmarks.kt
@@ -1,10 +1,13 @@
 package subprojects.benchmarks
 
+import dsl.*
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.*
 import subprojects.*
 import subprojects.build.*
 import subprojects.release.publishing.*
+
+private const val MAVEN_LOCAL_PATH = "%system.teamcity.build.checkoutDir%/ktor-repository"
 
 object ProjectBenchmarks : Project({
     id("ProjectKtorBenchmarks")
@@ -12,9 +15,13 @@ object ProjectBenchmarks : Project({
 
     vcsRoot(VCSKtorBenchmarks)
 
+    buildType(BuildKtorForAllocationBenchmarks)
+
     buildType {
         id("AllocationTests")
         name = "Allocation tests"
+
+        val ktorVersion = BuildKtorForAllocationBenchmarks.depParamRefs["ktorBenchmarkVersion"].ref
 
         vcs {
             root(VCSCore, "+:.=>ktor")
@@ -32,14 +39,26 @@ object ProjectBenchmarks : Project({
 
         steps {
             gradle {
-                tasks = "$JVM_AND_COMMON_PUBLISH_TASK $EXCLUDE_DOKA_GENERATION -PreleaseVersion=1.0.0-BENCHMARKS"
-                workingDir = "ktor"
-                jdkHome = Env.JDK_LTS
-            }
-            gradle {
-                tasks = "test -PktorVersion=1.0.0-BENCHMARKS"
+                tasks = "test"
+                gradleParams = "-PktorVersion=$ktorVersion " +
+                    "-Dmaven.repo.local=$MAVEN_LOCAL_PATH"
                 workingDir = "ktor-benchmarks/allocation-benchmark"
                 jdkHome = Env.JDK_LTS
+            }
+        }
+
+        dependencies {
+            dependency(BuildKtorForAllocationBenchmarks) {
+                snapshot {
+                    reuseBuilds = ReuseBuilds.SUCCESSFUL
+                    onDependencyFailure = FailureAction.FAIL_TO_START
+                    onDependencyCancel = FailureAction.CANCEL
+                }
+                artifacts {
+                    buildRule = sameChainOrLastFinished()
+                    artifactRules = "ktor-repository.zip!** => ktor-repository"
+                    cleanDestination = true
+                }
             }
         }
 
@@ -80,4 +99,47 @@ object ProjectBenchmarks : Project({
             param("type", "BuildReportTab")
         }
     }
+})
+
+private object BuildKtorForAllocationBenchmarks : BuildType({
+    id("BuildKtorForAllocationBenchmarks")
+    name = "Build Ktor for allocation benchmarks"
+
+    vcs {
+        root(VCSCore)
+    }
+
+    artifactRules = "+:ktor-repository => ktor-repository.zip"
+
+    steps {
+        script {
+            name = "Set Ktor benchmark version"
+            scriptContent = bashScript(
+                """
+                ktorVersion="${'$'}(<VERSION)"
+                ktorVersion="${'$'}{ktorVersion%-SNAPSHOT}-BENCHMARKS.%build.counter%"
+                echo "##teamcity[buildNumber '${'$'}ktorVersion']"
+                echo "##teamcity[setParameter name='ktorBenchmarkVersion' value='${'$'}ktorVersion']"
+                """
+            )
+        }
+        gradle {
+            tasks = JVM_AND_COMMON_PUBLISH_TASK
+            gradleParams = "$EXCLUDE_DOKA_GENERATION " +
+                "-PreleaseVersion=%ktorBenchmarkVersion% " +
+                "-Dmaven.repo.local=$MAVEN_LOCAL_PATH"
+            jdkHome = Env.JDK_LTS
+        }
+    }
+
+    requirements {
+        agent(Agents.OS.Linux)
+    }
+
+    params {
+        param("ktorBenchmarkVersion", "")
+        param("system.teamcity.default.properties", "teamcity.default.properties")
+    }
+
+    defaultBuildFeatures(VCSCore.id)
 })
