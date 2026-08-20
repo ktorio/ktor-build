d232244d67cc5878e8b60c38808bc16eb7c481fa
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2026-08-20 15:55:01 +0200

    Actually include gradle params in test build

--- .teamcity/src/subprojects/build/generator/TestPluginRegistry.kt
+++ .teamcity/src/subprojects/build/generator/TestPluginRegistry.kt
@@ -4,6 +4,7 @@ import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.*
 import subprojects.*
 import subprojects.build.*
+import subprojects.build.core.GradleParams
 import subprojects.build.core.extraGradleParams
 
 object TestPluginRegistry : BuildType({
@@ -21,6 +22,7 @@ object TestPluginRegistry : BuildType({
         gradle {
             name = "Test plugin registry"
             tasks = ":test:test"
+            gradleParams = "--info --continue $GradleParams"
             jdkHome = Env.JDK_LTS
         }
     }
