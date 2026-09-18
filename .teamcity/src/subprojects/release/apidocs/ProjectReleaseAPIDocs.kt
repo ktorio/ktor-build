e2aabe6580d2a3c7a0715c8e876b63dd11ccd44f
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2026-09-18 18:32:48 +0200

    API Docs: Use Gradle build step to generate API docs

--- .teamcity/src/subprojects/release/apidocs/ProjectReleaseAPIDocs.kt
+++ .teamcity/src/subprojects/release/apidocs/ProjectReleaseAPIDocs.kt
@@ -27,12 +27,30 @@ object ProjectReleaseAPIDocs : Project({
             checkoutMode = CheckoutMode.ON_AGENT
         }
 
+        params {
+            param("versionsDirectory", "%teamcity.build.checkoutDir%/versions")
+        }
+
         steps {
             script {
-                name = "Generate static files for version and push changes to git"
+                name = "Prepare API docs build"
+                scriptContent = "./build_doc.sh \"%releaseVersion%\" prepare \"%versionsDirectory%\""
+            }
+
+            gradle {
+                name = "Generate API docs"
+                tasks = ":ktor-dokka:dokkaGenerate"
+                gradleParams = "-Pversion=%releaseVersion% " +
+                    "-Pktor.dokka.versionsDirectory=%versionsDirectory%"
+                workingDir = "ktor"
+                jdkHome = Env.JDK_LTS
+            }
+
+            script {
+                name = "Finalize API docs build and push changes to git"
                 scriptContent = """
                     set -eu
-                    ./build_doc.sh "%releaseVersion%"
+                    ./build_doc.sh "%releaseVersion%" finalize "%versionsDirectory%"
                     git config user.name "TeamCity"
                     git config user.email "teamcity@jetbrains.com"
                     git remote set-url origin "https://oauth2:${'$'}{GITHUB_TOKEN}@github.com/ktorio/api.ktor.io.git"
